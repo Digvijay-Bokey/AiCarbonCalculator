df4be67d49749e511c0b2c75d8ed58f22507f182
Vishruth Bharath <vishruthbharath@gmail.com>
2023-06-17 20:16:46 -0700

    error

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import itertools
 import statsmodels.api as sm
-from sklearn.impute import KNNImputer
 
 # Predefine selected columns from original dataset
 selected_columns = ['REGIONC', 'DIVISION', 'state_name', 'BA_climate', 'TYPEHUQ', 'YEARMADERANGE', 'WALLTYPE', 'SWIMPOOL', 'RECBATH', 'FUELTUB', 'RANGEFUEL', 'OUTGRILLFUEL', 'DWASHUSE', 'DRYRFUEL', 'EQUIPM', 'FUELHEAT', 'FUELH2O', 'MONEYPY']
@@ -16,10 +15,6 @@ new_df = original_df.head(2000)[selected_columns]
 # Filter rows based on the condition (hot tub or pool)
 condition = (new_df['SWIMPOOL'] == 1) | (new_df['RECBATH'] == 1)
 new_df = new_df[condition]
-
-# Impute missing values using KNN imputation
-imputer = KNNImputer(n_neighbors=5)
-new_df_imputed = pd.DataFrame(imputer.fit_transform(new_df), columns=new_df.columns)
 
 def calculate_carbon_footprint(row):
     # Get the values from the row
@@ -47,8 +42,8 @@ def calculate_carbon_footprint(row):
     
     return total_carbon_footprint
 
-# Apply the calculate_carbon_footprint function to each row in new_df_imputed
-new_df_imputed['CARBFTP'] = new_df_imputed.apply(calculate_carbon_footprint, axis=1)
+# Apply the calculate_carbon_footprint function to each row in new_df
+new_df['CARBFTP'] = new_df.apply(calculate_carbon_footprint, axis=1)
 
 # Save new dataframe to csv
-new_df_imputed.to_csv('selected_with_carbon_footprint.csv', index=False)
+new_df.to_csv('selected_with_carbon_footprint.csv', index=False)
