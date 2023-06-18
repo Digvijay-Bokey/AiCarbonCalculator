59bd6dd2b78c7eb015afd805b573f30c063196b8
Vishruth Bharath <vishruthbharath@gmail.com>
2023-06-17 17:12:55 -0700

    created csv parsing to new csv file

--- main.py
+++ main.py
@@ -1,51 +1,13 @@
-import torch
-import torch.nn as nn
-import numpy as np
-from sklearn import datasets
-import matplotlib.pyplot as plt
+import pandas as pd
 
-# 0) Prepare data
-X_numpy, y_numpy = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=4)
+# Read data
+original_df = pd.read_csv('recs2020_public_v3.csv')
 
-# cast to float Tensor
-X = torch.from_numpy(X_numpy.astype(np.float32))
-y = torch.from_numpy(y_numpy.astype(np.float32))
-y = y.view(y.shape[0], 1)
+# Create new dataframe with selected columns
+selected_columns = ['REGIONC']
+new_df = original_df[selected_columns]
 
-n_samples, n_features = X.shape
+# Save new dataframe to csv
+new_df.to_csv('new.csv', index=False)
 
-# 1) Model
-# Linear model f = wx + b
-input_size = n_features
-output_size = 1
-model = nn.Linear(input_size, output_size)
-
-# 2) Loss and optimizer
-learning_rate = 0.01
-
-criterion = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-
-# 3) Training loop
-num_epochs = 100
-for epoch in range(num_epochs):
-    # Forward pass and loss
-    y_predicted = model(X)
-    loss = criterion(y_predicted, y)
-
-    # Backward pass and update
-    loss.backward()
-    optimizer.step()
-
-    # zero grad before new step
-    optimizer.zero_grad()
-
-    if (epoch + 1) % 10 == 0:
-        print(f'epoch: {epoch + 1}, loss = {loss.item():.4f}')
-
-# Plot
-predicted = model(X).detach().numpy()
-
-plt.plot(X_numpy, y_numpy, 'ro')
-plt.plot(X_numpy, predicted, 'b')
-plt.show()
+print(new_df)
