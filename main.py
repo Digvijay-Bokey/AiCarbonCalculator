6a56f79bedbfa44255bf16a15633000eee714662
Vishruth Bharath <vishruthbharath@gmail.com>
2023-06-17 20:15:07 -0700

    created prediction for carbon footprints using sarimax

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
+import itertools
 import statsmodels.api as sm
+from sklearn.impute import KNNImputer
 
 # Predefine selected columns from original dataset
 selected_columns = ['REGIONC', 'DIVISION', 'state_name', 'BA_climate', 'TYPEHUQ', 'YEARMADERANGE', 'WALLTYPE', 'SWIMPOOL', 'RECBATH', 'FUELTUB', 'RANGEFUEL', 'OUTGRILLFUEL', 'DWASHUSE', 'DRYRFUEL', 'EQUIPM', 'FUELHEAT', 'FUELH2O', 'MONEYPY']
@@ -9,35 +11,44 @@ selected_columns = ['REGIONC', 'DIVISION', 'state_name', 'BA_climate', 'TYPEHUQ'
 original_df = pd.read_csv('recs2020_public_v3.csv')
 
 # Create new dataframe with selected columns and the condition
-new_df = original_df[selected_columns]
+new_df = original_df.head(2000)[selected_columns]
 
 # Filter rows based on the condition (hot tub or pool)
 condition = (new_df['SWIMPOOL'] == 1) | (new_df['RECBATH'] == 1)
 new_df = new_df[condition]
 
-# Calculate the Mean Squared Error (MSE) for each row
-def calculate_mse(row):
-    # Get relevant data points and handle zeros
-    data = row[['TYPEHUQ', 'YEARMADERANGE', 'WALLTYPE', 'SWIMPOOL', 'RECBATH', 'FUELTUB', 'RANGEFUEL', 'OUTGRILLFUEL', 'DWASHUSE', 'DRYRFUEL', 'EQUIPM', 'FUELHEAT', 'FUELH2O', 'MONEYPY']]
-    numeric_columns = data.select_dtypes(include=np.number).columns
-    data[numeric_columns] = data[numeric_columns].apply(lambda x: np.log1p(x) if x > 0 else x)
+# Impute missing values using KNN imputation
+imputer = KNNImputer(n_neighbors=5)
+new_df_imputed = pd.DataFrame(imputer.fit_transform(new_df), columns=new_df.columns)
+
+def calculate_carbon_footprint(row):
+    # Get the values from the row
+    values = row[['TYPEHUQ', 'YEARMADERANGE', 'WALLTYPE', 'SWIMPOOL', 'RECBATH', 'FUELTUB', 'RANGEFUEL', 'OUTGRILLFUEL', 'DWASHUSE', 'DRYRFUEL', 'EQUIPM', 'FUELHEAT', 'FUELH2O', 'MONEYPY']]
     
-    # Create SARIMA model and fit to data
-    model = sm.tsa.SARIMAX(data, order=(1, 0, 1), seasonal_order=(1, 0, 1, 12))
+    # Preprocess the values (handle zeros, log transformation, etc.)
+    transformed_values = []
+    for value in values:
+        if value <= 0:
+            transformed_values.append(np.nan)
+        else:
+            transformed_values.append(np.log1p(value))
+    
+    # Create the SARIMAX model with the desired parameters
+    model = sm.tsa.SARIMAX(transformed_values, order=(1, 0, 1), seasonal_order=(1, 0, 1, 12))
+    
+    # Fit the model to the values
     result = model.fit()
     
     # Predict the next value
-    prediction = result.predict(start=data.index[-1], end=data.index[-1] + pd.DateOffset(months=1))
+    prediction = result.predict(start=len(transformed_values), end=len(transformed_values))
     
-    # Calculate the Mean Squared Error (MSE)
-    mse = np.mean((np.expm1(prediction) - np.expm1(data.iloc[-1])) ** 2)
+    # Calculate the total carbon footprint
+    total_carbon_footprint = np.expm1(prediction)
     
-    return mse
+    return total_carbon_footprint
 
-# Add a new column to selected.csv with the Mean Squared Error (MSE) for each row
-new_df['MSE'] = new_df.apply(calculate_mse, axis=1)
+# Apply the calculate_carbon_footprint function to each row in new_df_imputed
+new_df_imputed['CARBFTP'] = new_df_imputed.apply(calculate_carbon_footprint, axis=1)
 
 # Save new dataframe to csv
-new_df.to_csv('selected_with_mse.csv', index=False)
-
-print(new_df)
+new_df_imputed.to_csv('selected_with_carbon_footprint.csv', index=False)
