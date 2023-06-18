126130b8d12ffd80f993c31e3ba49fa526612bc4
Vishruth Bharath <vishruthbharath@gmail.com>
2023-06-18 00:52:42 -0700

    happiness

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.neural_network import MLPRegressor
 from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.keras.layers import Input, Dense
+from sklearn.preprocessing import StandardScaler
 
 # Predefine selected columns from original dataset
 selected_columns = ['REGIONC', 'DIVISION', 'state_name', 'BA_climate', 'TYPEHUQ', 'YEARMADERANGE', 'WALLTYPE', 'SWIMPOOL', 'RECBATH', 'FUELTUB', 'RANGEFUEL', 'OUTGRILLFUEL', 'DWASHUSE', 'DRYRFUEL', 'EQUIPM', 'FUELHEAT', 'FUELH2O', 'MONEYPY']
@@ -26,7 +29,7 @@ new_df['BA_climate'] = le.fit_transform(new_df['BA_climate'])
 
 # Filter rows based on the condition (hot tub or pool)
 condition = (new_df['SWIMPOOL'] == 1) | (new_df['RECBATH'] == 1)
-new_df = new_df[condition].head(1000)
+new_df = new_df[condition].head(500)
 
 # Change negative values and zeros to NaN for numeric columns
 #numeric_columns = new_df.select_dtypes(include=np.number).columns
@@ -44,8 +47,9 @@ def calculate_carbon_footprint(row):
         else:
             transformed_values.append(np.log1p(value))
     
+    
     # Create the SARIMAX model with the desired parameters
-    model = sm.tsa.SARIMAX(transformed_values, order=(1, 0, 1), seasonal_order=(1, 0, 1, 12))
+    model = sm.tsa.SARIMAX(transformed_values, order=(1, 0, 1), seasonal_order=(1, 0, 1, 12), enforce_stationarity=False)
     
     # Fit the model to the values
     result = model.fit()
@@ -72,15 +76,40 @@ dataset = new_df.values
 X = dataset[:, 0:18]
 Y = dataset[:, 18]
 
-# Impute missing values
-imputer = SimpleImputer()
-X_imputed = imputer.fit_transform(X)
+X_scale = preprocessing.MinMaxScaler().fit_transform(X)
 
-X_train, X_test, Y_train, Y_test = train_test_split(X_imputed, Y, test_size=0.4, random_state=0)
+X_train, X_test, Y_train, Y_test = train_test_split(X_scale, Y, test_size=0.2)
+X_val, X_test, Y_val, Y_test = train_test_split(X_test, Y_test, test_size=0.3)
 print(X_train.shape, X_test.shape, Y_train.shape, Y_test.shape)
 
+"""
+import tensorflow as tf 
+from tensorflow.python.keras.layers import Input, Dense 
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+model = Sequential([
+    Dense(32, activation='relu', input_shape = (12,)),
+    Dense(32, activation='relu'),
+    Dense(1, activation='sigmoid'),
+])
+                   
+model.compile(optimizer='sgd',
+              loss = 'binary_crossentropy',
+              metrics=['accuracy'])
+
+
+hist = model.fit(X_train, Y_test, 
+                 batch_size=32, epochs=100,
+                 validation_data=(X_val, Y_val))
+
+
+
+"""
+
 # Create and train the MLPRegressor model
-mlp = MLPRegressor(hidden_layer_sizes=(100, 100), activation='relu', solver='adam', random_state=0)
+mlp = MLPRegressor(hidden_layer_sizes=(100, 100), activation='relu', solver='adam', random_state=42)
 mlp.fit(X_train, Y_train)
 
 # Make predictions on the test set
@@ -97,6 +126,8 @@ print("Mean Squared Error:", mse)
 # Evaluate the model using R2 score
 r2 = metrics.r2_score(Y_test, pred)
 print("R2 Score:", r2)
+
+
 
 # Save new dataframe to csv
 new_df.to_csv('selected_with_carbon_footprint.csv', index=False)
