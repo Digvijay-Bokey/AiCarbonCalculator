15706c5ab4c5745642749e5a332d412959ad7ead
Vishruth Bharath <vishruthbharath@gmail.com>
2023-06-18 00:13:22 -0700

    add nn

--- main.py
+++ main.py
@@ -2,23 +2,35 @@ import pandas as pd
 import numpy as np
 import itertools
 import statsmodels.api as sm
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.neural_network import MLPRegressor
+from sklearn import metrics
 
 # Predefine selected columns from original dataset
 selected_columns = ['REGIONC', 'DIVISION', 'state_name', 'BA_climate', 'TYPEHUQ', 'YEARMADERANGE', 'WALLTYPE', 'SWIMPOOL', 'RECBATH', 'FUELTUB', 'RANGEFUEL', 'OUTGRILLFUEL', 'DWASHUSE', 'DRYRFUEL', 'EQUIPM', 'FUELHEAT', 'FUELH2O', 'MONEYPY']
 
-# Read data
+# Read data from csv file into dataframe
 original_df = pd.read_csv('recs2020_public_v3.csv')
 
 # Create new dataframe with selected columns and the condition
 new_df = original_df[selected_columns]
 
+le = preprocessing.LabelEncoder()
+
+new_df['REGIONC'] = le.fit_transform(new_df['REGIONC'])
+new_df['DIVISION'] = le.fit_transform(new_df['DIVISION'])
+new_df['state_name'] = le.fit_transform(new_df['state_name'])
+new_df['BA_climate'] = le.fit_transform(new_df['BA_climate'])
+
 # Filter rows based on the condition (hot tub or pool)
 condition = (new_df['SWIMPOOL'] == 1) | (new_df['RECBATH'] == 1)
-new_df = new_df[condition].head(50)
+new_df = new_df[condition].head(1000)
 
 # Change negative values and zeros to NaN for numeric columns
-numeric_columns = new_df.select_dtypes(include=np.number).columns
-new_df[numeric_columns] = new_df[numeric_columns].where(new_df[numeric_columns] > 0, np.nan)
+#numeric_columns = new_df.select_dtypes(include=np.number).columns
+#new_df[numeric_columns] = new_df[numeric_columns].where(new_df[numeric_columns] > 0, np.nan)
 
 def calculate_carbon_footprint(row):
     # Get the values from the row
@@ -48,6 +60,43 @@ def calculate_carbon_footprint(row):
 
 # Apply the calculate_carbon_footprint function to each row in new_df
 new_df['CARBFTP'] = new_df.apply(calculate_carbon_footprint, axis=1)
+
+# Cap carbon footprint to 15
+new_df['CARBFTP'][new_df['CARBFTP'] > 15] = 15
+
+# Scale the carbon footprint to 100
+new_df.loc[:, 'CARBFTP'] = new_df['CARBFTP'] / 15 * 100
+
+# Divide dataset into training and testing datasets
+dataset = new_df.values
+X = dataset[:, 0:18]
+Y = dataset[:, 18]
+
+# Impute missing values
+imputer = SimpleImputer()
+X_imputed = imputer.fit_transform(X)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X_imputed, Y, test_size=0.4, random_state=0)
+print(X_train.shape, X_test.shape, Y_train.shape, Y_test.shape)
+
+# Create and train the MLPRegressor model
+mlp = MLPRegressor(hidden_layer_sizes=(100, 100), activation='relu', solver='adam', random_state=0)
+mlp.fit(X_train, Y_train)
+
+# Make predictions on the test set
+pred = mlp.predict(X_test)
+
+# Evaluate the model using Mean Absolute Error (MAE)
+mae = metrics.mean_absolute_error(Y_test, pred)
+print("Mean Absolute Error:", mae)
+
+# Evaluate the model using Mean Squared Error (MSE)
+mse = metrics.mean_squared_error(Y_test, pred)
+print("Mean Squared Error:", mse)
+
+# Evaluate the model using R2 score
+r2 = metrics.r2_score(Y_test, pred)
+print("R2 Score:", r2)
 
 # Save new dataframe to csv
 new_df.to_csv('selected_with_carbon_footprint.csv', index=False)
