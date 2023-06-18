f8b56a909257ab2b23b9ee6b234b11b233e78dd6
TTPBoss <64545935+TTPBoss@users.noreply.github.com>
2023-06-17 17:19:09 -0700

    Linear pogression

---
+++ Data/Stats/igdata.js
@@ -0,0 +1,25 @@
+/*
+
+Travel Habits
+Home energy use
+diet Choices
+consumer habits
+Waste production
+
+Columns used (Code : info)
+
+DIVISION	Char	Census Division, Mountain Division is divided into North and South for RECS purposes
+state_name	Char	State Name
+BA_climate	Char	Building America Climate Zone
+TYPEHUQ	Num	Type of housing unit
+YEARMADERANGE	Num	Range when housing unit was built
+WALLTYPE	Num	Major outside wall material
+SWIMPOOL	Num	Has swimming pool
+union WITH
+RECBATH	Num	Have hot tub
+FUELTUB	Num	Fuel used for heating hot tub
+RANGEFUEL	Num	Fuel used by range
+OUTGRILLFUEL	Num	Fuel used by outdoor grill
+
+
+ */
